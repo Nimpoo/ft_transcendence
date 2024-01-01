27f47b5c3869b71c02d0c411097265e73e549dee
Nimpoo <marwan.ayoub.pro@gmail.com>
2024-01-01 21:17:51 +0100

    `layout.tsx`: adding header component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,17 @@
 "use strict"
 
 import { Metadata } from "next"
+import { Ubuntu } from "next/font/google"
 
 import "tailwindcss/tailwind.css"
 import "@/styles/Background.css"
+
+import Header from "@/components/Header"
+
+const ubu = Ubuntu ({
+	subsets: ["latin"],
+	weight: "500"
+})
 
 export const metadata: Metadata = {
 	title: "ft_transcendence",
@@ -19,8 +27,9 @@ function RootLayout({
 		<html lang="en">
 			<head>
 			</head>
-			<body className={ "bg-[url('/bg.png')] bg-cover bg-fixed bg-center bg-no-repeat text-white" }>
+			<body className={ "bg-[url('/bg.png')] bg-cover bg-fixed bg-center bg-no-repeat text-white " + ubu.className }>
 				<div className="m-auto max-w-7xl">
+          <Header />
 					{children}
 					<div id="gradient-bg">
 						<div id="gradient-container">
