45a8e27f85e31dd4acfbddaf44e68f3060b1fd0b
noalexan <noalexan@student.42nice.fr>
2024-01-07 20:15:58 +0100

    upgraded login method

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("connect/", views.connect, name='connect')
+  path('connect/', views.connect, name='connect'),
 ]
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,16 @@
 "use strict"
 "use client"
 
+import { signIn, signOut, useSession } from "next-auth/react"
+
 function Home(): JSX.Element {
+	const { data: session } = useSession()
 
 	return (
 		<main>
-
+			nickname: {session?.user?.name}
+			<button onClick={() => signIn()}>sign in</button>
+			<button onClick={() => signOut()}>sign out</button>
 		</main>
 	)
 }
