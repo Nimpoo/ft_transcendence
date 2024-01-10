81fc05355f8693243f3aa630804f13284c190e94
Nimpoo <marwan.ayoub.pro@gmail.com>
2024-01-11 00:52:46 +0100

    `profile/page.tsx`: layout

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+function Profile(): React.JSX.Element {
+	return (
+		<main className="flex flex-row w-full h-[39rem] justify-between">
+			<div className="flex flex-col w-[310px] justify-between">
+				<div className="h-[48%] rounded-3xl bg-[#D9D9D9] shadow-[5px_5px_0_1px_rgba(0,0,0,0.25)] ">
+
+				</div>
+				<div className="h-[48%] rounded-3xl bg-[#D9D9D9] shadow-[5px_5px_0_1px_rgba(0,0,0,0.25)]">
+
+				</div>
+			</div>
+
+			<div className="w-[620px] rounded-3xl bg-[#D9D9D9] shadow-[5px_5px_0_1px_rgba(0,0,0,0.25)]">
+
+			</div>
+
+			<div className="w-[310px] rounded-3xl bg-[#D9D9D9] shadow-[5px_5px_0_1px_rgba(0,0,0,0.25)]">
+
+			</div>
+		</main>
+	)
+}
+
+export default Profile
