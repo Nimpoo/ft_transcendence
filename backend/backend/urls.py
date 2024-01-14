c15a14f4626dbca6730ba2eede069137dff4bbcc
noalexan <noalexan@student.42nice.fr>
2024-01-14 02:24:36 +0100

    turn views into class (new auth method) and randomize `admin` uri

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@
 URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from backend import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URI, admin.site.urls),
     path('users/', include('users.urls')),
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.utils import timezone
 from random_username.generate import generate_username
+
 
 def random_username():
   return generate_username()[0]
 
-# Create your models here.
 class User(models.Model):
   nickname = models.CharField(max_length=30, default=random_username)
 
@@ -13,6 +12,9 @@ class User(models.Model):
   github_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
   discord_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
 
-  dfa_secret = models.CharField(max_length=50, null=True)
+  dfa_secret = models.CharField(max_length=50, blank=True, null=True)
 
   created_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.nickname
