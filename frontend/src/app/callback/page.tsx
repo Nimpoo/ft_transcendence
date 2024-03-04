4154e0b87dab2cb4b43219d236684fc8b84d62e2
Nimpoo <marwan.ayoub.pro@gmail.com>
2024-03-04 15:48:38 +0100

    Session cookie set up

--- frontend/src/app/callback/page.tsx
+++ frontend/src/app/callback/page.tsx
@@ -3,9 +3,12 @@
 import { redirect, useSearchParams } from "next/navigation"
 import { getAccessToken } from "./api"
 import { useEffect } from "react"
+import { useCookies } from "react-cookie"
 
 function Callback(): React.JSX.Element {
 	const searchParams = useSearchParams()
+
+	const [cookies, setCookie] = useCookies(["session"])
 
 	useEffect(() => {
 		const code = searchParams.get("code")
@@ -18,10 +21,21 @@ function Callback(): React.JSX.Element {
 						method: "POST",
 						body: `{"token":"${token}"}`,
 					},
-				).then(response => response.json()).then(data => console.log(data)) // TODO: Set cookie AND redirect to homepage ("/")
+				)
+				.then(response => response.json())
+				.then(data => {
+					setCookie("session", data["access_token"], {
+						// httpOnly: true,
+						secure: true,
+						maxAge: 60 * 60 * 24 * 7,
+						path: "/",
+					})
+					console.log(data)
+					console.log(cookies.session)
+				}) // TODO: Set cookie AND redirect to homepage ("/")
 			})
 		}
-	}, [])
+	}, [cookies.session, setCookie, searchParams])
 
 	return <></>
 }
