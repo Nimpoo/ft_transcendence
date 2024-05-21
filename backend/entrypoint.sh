59a80543c0d287c548b7c32233ed2dc1fc964f86
noalexan <noalexan@student.42nice.fr>
2024-05-21 18:11:39 +0200

    Make `pong` dependent on healthy state of `redis` and `postgres`

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-until nc -z -w30 postgres 5432
-do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
-
-until nc -z -w30 redis 6379
-do
-  echo "Waiting for Redis..."
-  sleep 1
-done
-
 python manage.py makemigrations users friends chat
 python manage.py migrate
 daphne backend.asgi:application -b pong
