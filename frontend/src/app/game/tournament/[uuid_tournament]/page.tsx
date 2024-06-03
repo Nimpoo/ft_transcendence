5ec39fd797c6eb94a10c785ee01d5a0cc362aa4a
Justine Munoz <justineemunozz@gmail.com>
2024-06-03 21:16:05 +0200

    bracket in tournament page

--- frontend/src/app/game/tournament/[uuid_tournament]/page.tsx
+++ frontend/src/app/game/tournament/[uuid_tournament]/page.tsx
@@ -10,6 +10,8 @@ import { useGame } from "@/providers/Game"
 import toast from "react-hot-toast"
 
 import "@/styles/Rainbow.css"
+import "@/styles/global.css"
+import "@/styles/Tournament.css"
 
 const ubu = Ubuntu ({
 	subsets: ["latin"],
@@ -39,25 +41,46 @@ function TournamentRoom(): React.JSX.Element | null {
 	}, [participants.length, router])
 
 	return (
-		<>
+		<div className="mt-auto">
+			<div style={{display: "flex", justifyContent: "center"}}>
+				<span className={ `stroke tournament-title ${ubu.className}` }>TOURNAMENT</span>
+			</div>
 			{participants && (
-				<div>
-					{participants[0]
-						? <h1>{participants[0]}</h1>
-						: <h1>Waiting for a participant ...</h1>
-					} {participants[1]
-						? <h1>{participants[1]}</h1>
-						: <h1>Waiting for a participant ...</h1>
-					} {participants[2]
-						? <h1>{participants[2]}</h1>
-						: <h1>Waiting for a participant ...</h1>
-					} {participants[3]
-						? <h1>{participants[3]}</h1>
-						: <h1>Waiting for a participant ...</h1>
-					}
+				<div className="bracket">
+					<div className="round-left">
+						<div className="participant-top">
+							{participants[0]
+								? <h1 className="font-color">{participants[0]}</h1>
+								: <h1 className="font-color">0 Waiting for a participant ...</h1>
+							} 
+						</div>
+						<div className="participant">
+							{participants[1]
+								? <h1 className="font-color">{participants[1]}</h1>
+								: <h1 className="font-color">1 Waiting for a participant ...</h1>
+							}
+						</div>
+					</div>
+					<div className="lines-bracket-left"></div>
+					<div className="lines-bracket-middle"></div>
+					<div className="lines-bracket-right"></div>
+					<div className="round-right">
+						<div className="participant-top">
+							{participants[2]
+								? <h1 className="font-color">{participants[2]}</h1>
+								: <h1 className="font-color">2 Waiting for a participant ...</h1>
+							} 
+						</div>
+						<div className="participant">
+							{participants[3]
+								? <h1 className="font-color">{participants[3]}</h1>
+								: <h1 className="font-color">3 Waiting for a participant ...</h1>
+							}
+						</div>
+					</div>
 				</div>
 			)}
-		</>
+		</div>
 	)
 }
 
