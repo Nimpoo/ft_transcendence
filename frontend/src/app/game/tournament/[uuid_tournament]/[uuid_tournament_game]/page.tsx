b13244e9c39cdddf4cd154b60df6c1cfb858c728
Nimpoo <marwan.ayoub.pro@gmail.com>
2024-06-25 15:56:06 +0200

    Fluid addle for tournament

--- frontend/src/app/game/tournament/[uuid_tournament]/[uuid_tournament_game]/page.tsx
+++ frontend/src/app/game/tournament/[uuid_tournament]/[uuid_tournament_game]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Ubuntu } from "next/font/google"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { useRouter, usePathname } from "next/navigation"
 
 import { useSession } from "@/providers/Session"
@@ -21,6 +21,10 @@ const ubu = Ubuntu ({
 function GamingRoom(): React.JSX.Element | null {
 
 	const [begin, setBegin] = useState<boolean>(true)
+	const up = useRef<boolean>(false)
+	const down = useRef<boolean>(false)
+
+	const gameloop = useRef<NodeJS.Timeout>()
 
 	const { session, status } = useSession()
 	const { players, sendMessage, gameStatus } = useGame()
@@ -39,39 +43,55 @@ function GamingRoom(): React.JSX.Element | null {
 
 	useEffect(() => {
 		if (gameStatus === "in-game") {
-			const keyhandler = (e: any) => {
+			const keypresshandler = (e: any) => {
 				if (e.key === "w" || e.key === "W") {
-					if (sendMessage) {
-						sendMessage({
-							"type": "game.paddle",
-							"user": session?.display_name,
-							"id": session?.id.toString(),
-							"key": "up",
-							"player": `${players && players[0] === session?.display_name ? "1" : "2"}`,
-							"uuid": uuid,
-						})
-					}
+					up.current = true
 				} else if (e.key === "s" || e.key === "S") {
-					if (sendMessage) {
-						sendMessage({
-							"type": "game.paddle",
-							"user": session?.display_name,
-							"id": session?.id.toString(),
-							"key": "down",
-							"player": `${players && players[0] === session?.display_name ? "1" : "2"}`,
-							"uuid": uuid,
-						})
-					}
+					down.current = true
 				}
 			}
 
-			window.addEventListener("keydown", keyhandler)
+			const keyunpresshandler = (e: any) => {
+				if (e.key === "w" || e.key === "W") {
+					up.current = false
+				} else if (e.key === "s" || e.key === "S") {
+					down.current = false
+				}
+			}
+
+			window.addEventListener("keydown", keypresshandler)
+			window.addEventListener("keyup", keyunpresshandler)
 
 			return () => {
-				window.removeEventListener("keydown", keyhandler)
+				window.removeEventListener("keydown", keypresshandler)
+				window.removeEventListener("keydown", keyunpresshandler)
 			}
 		}
 	}, [gameStatus])
+
+	useEffect(() => {
+		if (gameStatus === "in-game") {
+			gameloop.current = setInterval(() => {
+				if (up.current && !down.current)
+				{
+					sendMessage({
+						"type": "game.paddle",
+						"key": "up",
+					})
+				}
+
+				else if (!up.current && down.current)
+				{
+					sendMessage({
+						"type": "game.paddle",
+						"key": "down",
+					})
+				}
+			}, 75)
+
+			return () => clearInterval(gameloop.current)
+		}
+	}, [gameStatus, session, players])
 
 	const LetsBegin = () => {
 		setBegin(false)
